181da7615fb8a5cdc588152daca2cb0eb9bf823c
Derek Burgman <dereekb@gmail.com>
2021-10-30 01:50:37 -0500

    Run (Checkpoint)

---
+++ server/d-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Used within the docker container to build the final distributable.
+npm run build
--- server/d-run.sh
+++ server/d-run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # Used within the docker container to run the server.
-echo 'Server'
+
+RUN_COMMAND=start:dev
+
+if [ "$DIST_SERVER_RUN" = "true" ]; then
+RUN_COMMAND=start
+fi
+
+if [ "$DEBUG_SERVER_RUN" = "true" ]; then
+RUN_COMMAND=start:debug
+fi
+
+npm run $RUN_COMMAND
